0a25b5bbce68074126296fac613e5a93742d2971
anjaneyuluk-mls <anjaneyulu.kinnara@metrolabsservices.com>
2022-12-28 16:26:23 +0530

    form and routing

--- test.js
+++ test.js
@@ -1,2 +1,17 @@
-const [myvalue0, myvalue1] = [1, 2];
-console.log(myvalue0, myvalue1);
+var arr = [
+  { id: 1, date: 2021 },
+  { id: 2, date: 2021 },
+  { id: 3, date: 2022 },
+  { id: 4, date: 2023 },
+];
+
+const dateMapiing = {};
+arr.forEach((item) => {
+  const date = item.date;
+  if (dateMapiing[date]) {
+    dateMapiing[date].push(item);
+  } else {
+    dateMapiing[date] = [item];
+  }
+});
+console.log(dateMapiing);
